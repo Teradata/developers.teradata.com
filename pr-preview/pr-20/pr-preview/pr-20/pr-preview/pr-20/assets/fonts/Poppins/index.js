51e0fb0f030dd4a2b7c7c17948b2bf69d55b234d
ObedVega <ObedVega@users.noreply.github.com>
2023-07-10 23:21:34 +0000

    Deploy preview for PR 20 🛫

---
+++ pr-preview/pr-20/pr-preview/pr-20/pr-preview/pr-20/assets/fonts/Poppins/index.js
@@ -0,0 +1,39 @@
+import PoppinsBlack from './Poppins-Black.ttf';
+import PoppinsBlackItalic from './Poppins-BlackItalic.ttf';
+import PoppinsBold from './Poppins-Bold.ttf';
+import PoppinsBoldItalic from './Poppins-BoldItalic.ttf';
+import PoppinsExtraBold from './Poppins-ExtraBold.ttf';
+import PoppinsExtraBoldItalic from './Poppins-ExtraBoldItalic.ttf';
+import PoppinsExtraLight from './Poppins-ExtraLight.ttf';
+import PoppinsExtraLightItalic from './Poppins-ExtraLightItalic.ttf';
+import PoppinsItalic from './Poppins-Italic.ttf';
+import PoppinsLight from './Poppins-Light.ttf';
+import PoppinsLightItalic from './Poppins-LightItalic.ttf';
+import PoppinsMedium from './Poppins-Medium.ttf';
+import PoppinsMediumItalic from './Poppins-MediumItalic.ttf';
+import PoppinsRegular from './Poppins-Regular.ttf';
+import PoppinsSemiBold from './Poppins-SemiBold.ttf';
+import PoppinsSemiBoldItalic from './Poppins-SemiBoldItalic.ttf';
+import PoppinsThin from './Poppins-Thin.ttf';
+import PoppinsThinItalic from './Poppins-ThinItalic.ttf';
+
+export default {
+  PoppinsBlack,
+  PoppinsBlackItalic,
+  PoppinsBold,
+  PoppinsBoldItalic,
+  PoppinsExtraBold,
+  PoppinsExtraBoldItalic,
+  PoppinsExtraLight,
+  PoppinsExtraLightItalic,
+  PoppinsLight,
+  PoppinsLightItalic,
+  PoppinsMedium,
+  PoppinsMediumItalic,
+  PoppinsRegular,
+  PoppinsItalic,
+  PoppinsSemiBold,
+  PoppinsSemiBoldItalic,
+  PoppinsThin,
+  PoppinsThinItalic,
+};
